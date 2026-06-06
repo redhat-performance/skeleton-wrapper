e611702e547cc88226440d3cb13cb875b6c4b55d
David Valin <dvalin@redhat.com>
2026-06-06 05:08:08 -0400

    Revert "Adding wrapper for valkey benchmark"
    This reverts commit f466dbb57f5dbc249015d6006d4de5171b448f47.
    
    Undo wrong push

--- skeleton/skeleton.sh
+++ skeleton/skeleton.sh
@@ -3,7 +3,7 @@
 #                         License
 #
 #=================================================
-# Copyright (C) June 4, 2026  David Valin dvalin@redhat.com
+# Copyright (C) <data>  <your name> <your email>
 #=================================================
 #
 # This program is free software; you can redistribute it and/or
@@ -25,19 +25,13 @@
 #
 
 #=================================================
-valkey_version="v1.00"
-version=1.00
-test_name=valkey
+commit="none"
+<test_name>_version="v1.01"
+test_name="<name of test>"
 #=================================================
 results_file="results_${test_name}.csv"
 arguments="$@"
 script_dir=$(realpath $(dirname $0))
-pcpdir=""
-
-#total_requests=600000
-total_requests=100000
-connections=50
-threads=""
 
 exit_out()
 {
@@ -84,7 +78,9 @@ export TOOLS_BIN
 usage()
 {
 	echo "Usage $1:"
-	echo add usage
+#=================================================
+	< Add your usage information >
+#=================================================
 	source $TOOLS_BIN/general_setup --usage
 	exit $E_USAGE
 }
@@ -173,84 +169,17 @@ source "$TOOLS_BIN/general_setup" "$@"
 popd 2> /dev/null
 # Gather hardware information
 $TOOLS_BIN/gather_data ${curdir}
-test_list=""
-
-execute_valkey()
-{
-	out_file=valkey_iter_${1}
-	test_list=""
-
-
-	run_threads=$(echo $threads | sed "s/,/ /g")
-	run_threads=1
-	for th in $run_threads;
-	do
-		if [[ $to_use_pcp -eq 1 ]]; then
-			start_pcp_subset
-		fi
-		start_time=$(retrieve_time_stamp)
-		valkey-benchmark -h 127.0.0.1 -p 6379 -n $total_requests -c $connections  --threads $th --csv  | sed "s/\"//g" | sed "s/ /_/g" | sed "s/)//g" | sed "s/(//g" > ${out_file}_${th}_threads.csv
-		end_time=$(retrieve_time_stamp)
-		if [[ $test_list == "" ]]; then
-			separ=""
-			echo file "${out_file}_${th}_threads.csv"
-			while IFS= read -r run_data
-			do
-				if [[ $run_data == *"test"* ]]; then
-					continue
-				fi
-				tst=$(echo $run_data | cut -d, -f 1 | sed "s/\"//g" | sed "s/ /_/g"| sed "s/)//g")
-				test_list=${tst}${separ}${test_list}
-				separ=' '
-			done < "${out_file}_${th}_threads.csv"
-		fi
-		if [[ $to_use_pcp -eq 1 ]]; then
-			results2pcp_add_value "iteration:${1}"
-			results2pcp_add_value "runlog:1"
-			results2pcp_add_value "numthreads:$th"
-			results2pcp_add_value "IterationsPerSec:0"
-			#
-			# Record the values for this thread.
-			#
-			for tst in $test_list
-			do
-				value=$(grep "^${tst}", ${out_file}_${th}_threads.csv  | cut -d',' -f2 | sed "s/\"//g" | cut -d'.' -f 1 )
-				results2pcp_add_value "${tst}:${value}"
-			done
-			results2pcp_add_value_commit
-			reset_pcp_om
-			stop_pcp_subset
-		fi
-	done
-
-	$TOOLS_BIN/test_header_info --front_matter --results_file ${out_file}.csv --host $to_configuration --sys_type $to_sys_type --tuned $to_tuned_setting --results_version $valkey_version --test_name $test_name --field_header "Test,RPS"
-	for tst in $test_list
-	do
-		value=$(grep "^$tst", *csv | cut -d',' -f2 | sed "s/\"//g" | sort -n | tail -1)
-		echo ${tst},${value},${start_time},${end_time} >> ${out_file}.csv
-	done
-	pwd
-	echo ${TOOLS_BIN}/csv_to_json $to_json_flags --csv_file ${out_file}.csv --output_file valkey_verify.json
-	${TOOLS_BIN}/csv_to_json $to_json_flags --csv_file ${out_file}.csv --output_file valkey_verify.json
-	test_rtc=$?
-	if [[ $test_rtc -ne 0 ]]; then
-		exit_out "${TOOLS_BIN}/csv_to_json $to_json_flags --csv_file ${out_file}.csv --output_file valkey_verify.json returned an an error" $test_rtc
-	fi
-	${TOOLS_BIN}/verify_results $to_verify_flags --schema_file $script_dir/results_schema.py --class_name valkey_Results --file valkey_verify.json
-	test_rtc=$?
-	if [[ $test_rtc -ne 0 ]]; then
-		echo Test failure detected: $out_File
-	fi
-}
 
 ARGUMENT_LIST=(
-	"commit"
-	"connections"
-	"threads"
-	"total_requests"
+#=================================================
+	Arguments that expect to have a value associated with it
+#=================================================
 )
 
 NO_ARGUMENTS=(
+#=================================================
+	Arguments that have no options
+#================================================
 	"usage"
 )
 
@@ -267,22 +196,9 @@ eval set --$opts
 
 while [[ $# -gt 0 ]]; do
 	case "$1" in
-		--commit)
-			commit=$2
-			shift 2
-		;;
-		--connections)
-			connections=$2
-			shift 2
-		;;
-		--threads)
-			threads=$2
-			shift 2
-		;;
-		--total_requests)
-			total_requests=$2
-			shift 2
-		;;
+#==================================================
+		Pull arguments
+#==================================================
 		--usage)
 			usage $0
 		;;
@@ -299,17 +215,13 @@ while [[ $# -gt 0 ]]; do
 	esac
 done
 
-if [[ $threads == "" ]]; then
-	cpus=$(nproc)
-	if [[ $cpus -lt 8 ]]; then
-		intervals=$cpus
-	else
-		intervals=8
-	fi
-	threads=$(${TOOLS_BIN}/generate_intervals --interval $intervals --max_value $cpus)
-fi
+#==================================================
+validation of arguments, if required.
+#==================================================
 
-package_tool --no_packages $to_no_pkg_install --wrapper_config $curdir/../valkey.json
+#==================================================
+package_tool --no_packages $to_no_pkg_install --wrapper_config $curdir/../<skeleton.json>
+#==================================================
 
 # Get PCP setup if we're using it
 if [[ $to_use_pcp -eq 1 ]]; then
@@ -317,29 +229,25 @@ if [[ $to_use_pcp -eq 1 ]]; then
 	setup_pcp
 	pcp_cfg=$TOOLS_BIN/pcp/default.cfg
 	pcpdir=/tmp/pcp_`date "+%Y.%m.%d-%H.%M.%S"`
-	echo "Start PCP"
-	echo start_pcp ${pcpdir}/ ${test_name} $pcp_cfg
-	start_pcp ${pcpdir}/ ${test_name} $pcp_cfg
-	echo returned from start_pcp
 fi
 
-rm -rf *csv
-for iter in $(seq 1 1 $to_times_to_run); do
-	execute_valkey $iter
-done
+#==================================================
+run your test.
+#=================================================
 
 # Shutdown PCP and clean up after ourselves
 if [[ $to_use_pcp -eq 1 ]]; then
         shutdown_pcp
 fi
-#$TOOLS_BIN/csv_to_json $to_json_flags --csv_file ${results_file} --output_file results_<test_name>.json
-#$TOOLS_BIN/verify_results $to_verify_flags --schema_file $script_dir/../result_schema.py --class_name <classname> --file results_<test_name>.json
+#=================================================
+$TOOLS_BIN/csv_to_json $to_json_flags --csv_file ${results_file} --output_file results_<test_name>.json
+$TOOLS_BIN/verify_results $to_verify_flags --schema_file $script_dir/../result_schema.py --class_name <classname> --file results_<test_name>.json
 
 
-#${TOOLS_BIN}/save_results --curdir $curdir --home_root $to_home_root <arguments>
+${TOOLS_BIN}/save_results --curdir $curdir --home_root $to_home_root <arguments>
 
-#example
+example
 
-#${TOOLS_BIN}/save_results --curdir $curdir --home_root $to_home_root <arguments> --other_files "*_summary,run*log,test_results_report,${pcpdir}" --results $results_file --test_name coremark --tuned_setting=$to_tuned_setting --version $<test_name>_version --user $to_user
+${TOOLS_BIN}/save_results --curdir $curdir --home_root $to_home_root <arguments> --other_files "*_summary,run*log,test_results_report,${pcpdir}" --results $results_file --test_name coremark --tuned_setting=$to_tuned_setting --version $<test_name>_version --user $to_user
 #=================================================
 exit $E_SUCCESS
